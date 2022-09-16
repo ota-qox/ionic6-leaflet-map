52de38e4da23ec4773955a44d2f7946090983efc
Shogo Ota <ota@qox.jp>
2022-09-16 16:21:03 +0900

    add: leaflet

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LatLngExpression, Map, marker, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  map: Map;
+
   constructor() {}
 
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    const qoxLatLng: LatLngExpression = [33.235948786009615, 131.6109924884563];
+    // In setView add LatLng and zoom
+    this.map = new Map('mapId').setView(qoxLatLng, 10);
+    tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    marker([33.186550, 131.434078])
+      .addTo(this.map)
+      .bindPopup('有限会社麻生本店');
+    marker([33.597116, 131.195112])
+      .addTo(this.map)
+      .bindPopup('旭酒造株式会社')
+      .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 }
